91784c651d7bf9497ab141ef64c2cf0b2c2048e8
Jorge Jiménez <154094092+JorgeAJT@users.noreply.github.com>
2024-05-10 19:39:28 +0000

    added user endpoints

--- src/api/routes.py
+++ src/api/routes.py
@@ -20,3 +20,34 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# USER ENDPOINTS
+@api.route('/users', methods=['GET'])
+def get_users():
+    all_users = User.query.all()
+    all_users_list = list(map(lambda user: user.serialize(),all_users))
+
+    return jsonify(all_users_list), 200
+
+@api.route('/users', methods=['POST'])
+def add_user():
+    user_data = request.json
+    required_properties = ["email", "password"]
+
+    for prop in required_properties:
+        if prop not in user_data: return jsonify({"Error": f"The property '{prop}' was not properly written"}), 400 
+
+    user_to_add = User(**user_data)
+    db.session.add(user_to_add)
+    db.session.commit()
+
+    return jsonify(user_to_add.serialize()), 200
+
+@api.route('/users/<int:user_id>', methods=['DELETE'])
+def del_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if not user: return jsonify({"Error": f"The ID '{user_id}' is not associated with any user"}), 400 
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify({"Deleted": f"The user '{user.email}' was deleted successfully"}), 200 
